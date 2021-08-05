0ab13b1995b9c1304ab43f3bf71b2f5852adfdfb
fahmi ahmad <fahmiahmad@tutanota.com>
2021-08-05 21:30:02 +0700

    finished chapter 5

--- haskell/c05/ch5.hs
+++ haskell/c05/ch5.hs
@@ -135,4 +135,168 @@ flip'' f = \x y -> f y x
 
 -- I Fold You So
 --
+-- a fold takes a binary function, a starting value, and a list
+-- to fold up.
 --
+
+-- left folds with foldl
+--
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+-- sum' :: (Num a) => [a] -> a
+-- sum' = foldl (+) 0
+
+-- right folds with foldr
+--
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+-- If we’re mapping (+3) to [1,2,3] , we approach the list from the right
+-- side. We take the last element, which is 3 , and apply the function to it, which
+-- gives 6 . Then we prepend it to the accumulator, which was [] . 6:[] is [6]
+
+-- map' :: (a -> b) -> [a] -> [b]
+-- map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+-- One big difference between the two types of folds is that right folds work
+-- on infinite lists, whereas left ones don’t!
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+-- the fold and foldr 1 functions
+-- They assume the first (or last) element of 
+-- the list to be the starting accumulator,
+-- and then start the fold with the element next to it.
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+-- Because they depend on the lists they’re called with having at least one
+-- element, these functions cause runtime errors if called with empty lists. 
+-- foldl and foldr , on the other hand, work fine with empty lists.
+--
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+-- reverse' :: [a] -> [a]
+-- reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+-- another way to look at folds
+--
+-- when we right fold over the list [3,4,5,6],
+-- we're essentially doing this:
+-- f 3 (f 4 (f 5 (f 6 z)))
+--
+-- doing left fold over that list with g as the binary
+-- function and z as the accumulator is the equivalent of this:
+-- g (g (g (g z 3) 4) 5) 6
+-- flip (:) (flip (:) (flip (:) (flip (:) [] 3) 4) 5) 6
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
+
+-- Scans
+--
+-- the scanl and scanr functions are like foldl and foldr,
+-- except they report all the intermediate accumulator states
+-- in the form of a list.
+-- the scanl1 and scanr1 functions are analogous to foldl1 and foldr1
+--
+-- scanl (+) 0 [3,5,2,1]
+-- scanr (+) 0 [3,5,2,1]
+-- scanl1 (\acc x -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
+
+-- how many elements does it take for the sum of the square 
+-- roots of all natural numbers to exceed 1000 ?
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+-- Function application with $
+--
+-- ($) :: (a -> b) -> a -> b
+-- f $ x = f x
+--
+-- hereas normal function application
+-- (putting a space between two things) has a really high
+-- precedence, the $ function has the lowest precedence
+--
+-- sum (filter (> 10) (map (*2) [2..10]))
+-- sum $ filter (> 10) $ map (*2) [2..10]
+
+-- The $ function is right-associative, meaning that something like f $ g $ x
+-- is equivalent to f $ (g $ x) .
+--
+-- map ($ 3) [(4+), (10*), (^2), sqrt]
+
+
+-- function composition
+--
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
+-- f . g = \x -> f (g x)
+--
+-- One use for function composition is
+-- making functions on the fly to pass to other
+-- functions.
+
+-- map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,19,24]
+-- 
+-- alternative:
+--
+-- map (negate . abs) [5,-3,-6,7,-3,2,19,24]
+
+-- function composition is right-associative
+-- f (g (z x)) is equivalent to (f . g . z) x
+
+-- map (\xs -> negate (sum (tail xs))) [[1..5],[3..6],[1..7]]
+-- alternative:
+--
+-- map (negate . sum . tail) [[1..5],[3..6],[1..7]]
+
+-- Function composition with multiple parameters
+--
+-- sum (replicate 5 (max 6.7 8.9))
+-- (sum . replicate 5) max 6.7 8.9
+-- sum replicate 5 $ max 6.7 8.9
+--
+-- replicate 2 (product (map (*3) (zipWith max [1,2] [4,5])))
+-- replicate 2 . product . map (*3) $ zipWith max [1,2] [4,5]
+--
+
+-- Point-free style
+--
+-- another common use of function composition is defining functions 
+-- in the point-free style
+--
+-- sum' :: (Num a) => [a] -> a
+-- sum' xs = foldl (+) 0 xs
+--
+-- because of currying, we can omit the xs on both sides,
+-- since calling foldl (+) 0 creates a function that takes
+-- a list
+--
+-- sum' :: (Num a) => [a] -> a
+-- sum' = foldl (+) 0
+-- 
+-- example:
+-- fn x = ceiling (negate (tan (cos (max 50 x))))
+--
+-- fn = ceiling . negate . tan . cos . max 50
+--
+-- oddSquareSum :: Integer
+-- oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+--
+-- oddSquareSum :: Integer
+-- oddSquareSum = sum . takeWhile (<10000) . filter odd $ map (^2) [1..]
+
+
+
+
