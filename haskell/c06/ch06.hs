7c1ad1a6450c051a2c7f8a17543b58690e2b3c77
fahmi ahmad <fahmiahmad@tutanota.com>
2021-08-24 09:53:41 +0700

    update

--- haskell/c06/ch06.hs
+++ haskell/c06/ch06.hs
@@ -1,4 +1,11 @@
 -- Modules
 --
 
+import Data.List
+-- import Data.List (nub, sort)
+-- import Data.List hiding (nub)
+-- import qualified Data.Map
+-- import qualified Data.Map as M
 
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
